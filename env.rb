804d0e93cca45f39d041c18ef5eb6adfe3be9609
a-munakata <a.munakata@4digit.jp>
2013-05-29 16:59:42 +0900

    任意の保存先ファイルを指定、dataの複数形と単数形
    任意の保存先ファイルを指定出来るようにしました。
    dataの単数形はdatum。。

--- env.rb
+++ env.rb
@@ -1,16 +1,16 @@
 require 'yaml'
 
-datas 	= YAML.load_file("application.yml")
-file		= open(".bash_profile", "a")
+data 	= YAML.load_file(ARGV[0])
+file		= open(ARGV[1], "a")
 options = ["development", "test", "bench", "staging", "production"]
 
-for data in datas
-	if data[0] != "development" && data[0] != "test" && data[0] != "bench" && data[0] != "staging" && data[0] != "production"	
-			file.puts data[0] + "=" + datas[data[0]]
+data.each do |datum|
+	if datum[0] != "development" && datum[0] != "test" && datum[0] != "bench" && datum[0] != "staging" && datum[0] != "production"	
+			file.puts datum[0] + "=" + data[datum[0]]
 	else
 		options.each do |option|
-			if data[0][option] == ARGV[0]
-				for hash in datas[option]
+			if datum[0][option] == ARGV[2]
+				for hash in data[option]
 					file.puts hash[0] + "=" + hash[1]
 				end
 			end			
