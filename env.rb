f3f3f2a7867b38812126454520c8013f59235b70
a-munakata <a.munakata@4digit.jp>
2013-05-29 18:07:28 +0900

    keyが既にに存在するか？
    keyが既にに存在したら上書きをしない。

--- env.rb
+++ env.rb
@@ -1,20 +1,35 @@
 require 'yaml'
 
+
 data 	= YAML.load_file(ARGV[0])
-file		= open(ARGV[1], "a")
 options = ["development", "test", "bench", "staging", "production"]
 
+
+
 data.each do |datum|
+	key = datum[0]
 	if datum[0] != "development" && datum[0] != "test" && datum[0] != "bench" && datum[0] != "staging" && datum[0] != "production"	
-			file.puts datum[0] + "=" + data[datum[0]]
+		if ENV[datum[0]].nil?
+			puts datum[0] + "=" + data[datum[0]]			
+		else
+			puts "ERROR! the key exists : #{datum[0]}"
+			break;
+		end
 	else
 		options.each do |option|
 			if datum[0][option] == ARGV[2]
 				for hash in data[option]
-					file.puts hash[0] + "=" + hash[1]
+					if ENV[hash[0]].nil?
+							puts hash[0] + "=" + hash[1]
+						else
+							puts "ERROR! the key exists : #{hash[0]}"
+							break;
+					end
 				end
 			end			
 		end
 	end		
 end
-file.close
+
+
+
