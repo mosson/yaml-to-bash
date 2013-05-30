5bc272f0d7dd7b6534883ca2310a1dc622fcdc3f
a-munakata <a.munakata@4digit.jp>
2013-05-30 10:05:24 +0900

    判断条件をデータのクラスに変更
    変更しました。

--- env.rb
+++ env.rb
@@ -1,35 +1,23 @@
 require 'yaml'
 
-
 data 	= YAML.load_file(ARGV[0])
-options = ["development", "test", "bench", "staging", "production"]
-
-
+items = ["development", "test", "bench", "staging", "production"]
 
 data.each do |datum|
-	key = datum[0]
-	if datum[0] != "development" && datum[0] != "test" && datum[0] != "bench" && datum[0] != "staging" && datum[0] != "production"	
-		if ENV[datum[0]].nil?
-			puts datum[0] + "=" + data[datum[0]]			
-		else
-			puts "ERROR! the key exists : #{datum[0]}"
-			break;
-		end
-	else
-		options.each do |option|
-			if datum[0][option] == ARGV[2]
-				for hash in data[option]
-					if ENV[hash[0]].nil?
-							puts hash[0] + "=" + hash[1]
-						else
-							puts "ERROR! the key exists : #{hash[0]}"
-							break;
-					end
-				end
-			end			
-		end
-	end		
+	raise "ERROR! the key exists : #{datum[0]}" unless ENV[datum[0]].nil?
+	puts "#{datum[0]}=#{datum[1]}" if datum[1].instance_of?(String)
 end
 
-
-
+items.each do |item|
+	raise "No such an option" unless items.include? ARGV[1]
+	if item == ARGV[1]
+		data[item].each do |hash|
+			if ENV[hash[0]].nil?
+					puts "#{hash[0]}=#{hash[1]}"
+				else
+					raise "ERROR! the key exists : #{hash[0]}"
+					break;
+			end
+		end
+	end			
+end	
