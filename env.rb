9866b77f61da8a65ad435cc06e0b401d4257df69
a-munakata <a.munakata@4digit.jp>
2013-05-31 13:28:43 +0900

    MyErrorクラスを追加しました。

--- env.rb
+++ env.rb
@@ -3,27 +3,31 @@ require 'yaml'
 arr = Array.new
 items = Array.new
 
+class MyError < RuntimeError; end
+ 
+raise MyError.new "ERROR! File doesn't exsists : #{ARGV[0]}" unless File.exist? ARGV[0]
+
 begin
 if ARGV.length == 2
 	data 	= YAML.load_file(ARGV[0])
 	environment = ARGV[1]
 else
-	raise "Invalid arguments specified\nUsage : sample.rb <cfg YAML file> [option]"	
+	raise MyError.new "Invalid arguments specified\nUsage : sample.rb <cfg YAML file> [option]"
 end
 
 data.each {|key, val| items.push key }
-raise "ERROR! Invalid option : #{ARGV[1]}" unless items.include?(ARGV[1])
+raise MyError.new "ERROR! Invalid option : #{ARGV[1]}" unless items.include?(ARGV[1])
 
 # オプション無し
 data.each do |key, value|
-	raise "ERROR! the key exists : #{key}" unless ENV[key].nil?
+	raise MyError.new  "ERROR! the key exists : #{key}" unless ENV[key].nil?
 	arr.push "#{key}=#{value}" unless value.instance_of?(Hash)
 end
 	
 # オプションあり
 if data.has_key?(environment)	
 	data[environment].each do |key, value|
-		raise "ERROR! the key exists : #{key}" unless ENV[key].nil?
+		raise MyError.new  "ERROR! the key exists : #{key}" unless ENV[key].nil?
 		arr.push "#{key}=#{value}"
 	end
 end
@@ -31,5 +35,9 @@ end
 puts arr
 
 rescue SystemExit => e
-	puts e
+  if e.instance_of? MyError
+    puts "#{e}(MyError)"
+  else
+    puts e
+  end
 end
