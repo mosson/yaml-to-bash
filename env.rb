820ead697f03102fff26f89e247b89ddab99b284
a-munakata <a.munakata@4digit.jp>
2013-05-31 10:26:35 +0900

    変更しました。

--- env.rb
+++ env.rb
@@ -1,25 +1,35 @@
 require 'yaml'
 
-data 	= YAML.load_file(ARGV[0])
+arr = Array.new
 items = Array.new
 
-data.each do |dataKey, dataVal|
-	items.push(dataKey) if dataVal.instance_of?(Hash)
-	raise "ERROR! the key exists : #{dataKey}" unless ENV[dataKey].nil?
-	puts "#{dataKey}=#{dataVal}" if dataVal.instance_of?(String)
-	
-	if dataVal.instance_of?(Hash)
-		if dataKey == ARGV[1]
-			dataVal.each do |key, val|								
-				raise "ERROR! the key exists : #{key}" unless ENV[key].nil?
-				puts "#{key}=#{val}"
-			end
-		end
-	end		 
+begin
+if ARGV.length == 2
+	data 	= YAML.load_file(ARGV[0])
+	environment = ARGV[1]
+else
+	raise "Invalid arguments specified\nUsage : sample.rb <cfg YAML file> [option]"	
 end
 
-raise "ERROR! No such an option : #{ARGV[1]}" unless items.include?(ARGV[1])
+data.each {|key, val| items.push key }
+raise "ERROR! Invalid option : #{ARGV[1]}" unless items.include?(ARGV[1])
 
+# オプション無し
+data.each do |key, value|
+	raise "ERROR! the key exists : #{key}" unless ENV[key].nil?
+	arr.push "#{key}=#{value}" unless value.instance_of?(Hash)
+end
+	
+# オプションあり
+if data.has_key?(environment)	
+	data[environment].each do |key, value|
+		raise "ERROR! the key exists : #{key}" unless ENV[key].nil?
+		arr.push "#{key}=#{value}"
+	end
+end
 
+puts arr
 
-
+rescue SystemExit => e
+	puts e
+end
